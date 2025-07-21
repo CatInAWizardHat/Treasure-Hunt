66f9cf70a8f37d84224b21f28b1803a035750a62
CatInAWizardHat <125680751+CatInAWizardHat@users.noreply.github.com>
2025-07-21 15:07:07 -0700

    Created get_player() and add_player() functions

--- src/Game.py
+++ src/Game.py
@@ -2,8 +2,23 @@ from Board import Board
 from Player import Player
 from typing import List
 
+PLAYER_NAMES = ["One", "Two"]
+MAX_PLAYERS = 2
+
 
 class Game:
-    def __init__(self, board: Board) -> None:
+    def __init__(self, board: Board = Board(10, 4)) -> None:
         self.players: List[Player] = []
         self.board: Board = board
+        self.cnx: int = 0
+
+    def get_player(self) -> Player:
+        return self.players[self.cnx]
+
+    def add_player(self) -> None:
+        if self.cnx == MAX_PLAYERS:
+            return
+
+        new_player = Player(PLAYER_NAMES[self.cnx])
+        self.cnx += 1
+        self.players.append(new_player)
