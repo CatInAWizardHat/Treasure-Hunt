397863361a7ead4064fdefc8d8657ea9a326fea4
CatInAWizardHat <125680751+CatInAWizardHat@users.noreply.github.com>
2025-07-21 14:33:08 -0700

    Updated src/Player.py to use dataclass annotation, removed getter for score

--- src/Player.py
+++ src/Player.py
@@ -1,26 +1,20 @@
-class Player:
-    def __init__(self, name):
-        """
-        :param name: a name for the player from a prompt
-        """
-        self.name = name
-        self.score = 0
+from dataclasses import dataclass
 
-    def add_score(self, score):
+
+@dataclass
+class Player:
+    name: str
+    points: int = 0
+
+    def add_score(self, score: int) -> None:
         """
         :param score: the score to be added onto the current player object
         :return: nothing
         """
         self.score += score
 
-    def get_score(self) -> int:
-        """
-        :return: the current score value for the player
-        """
-        return self.score
-
-    def __str__(self):
+    def __str__(self) -> str:
         """
         :return: string representation of the player
         """
-        return str("Player: " + self.name + "\nScore is: " + str(self.get_score()))
+        return str(f"Player: {self.name}\nScore is: {self.score}")
