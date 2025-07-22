939d1bc7cdc754eb9eec5f26165f7547b29097e5
CatInAWizardHat <125680751+CatInAWizardHat@users.noreply.github.com>
2025-07-21 17:37:28 -0700

    Refactored main.py to remove most game-related functionality

--- src/Game.py
+++ src/Game.py
@@ -1,6 +1,6 @@
 from Board import Board
 from Player import Player
-from typing import List
+from typing import Dict
 
 PLAYER_NAMES = ["One", "Two"]
 MAX_PLAYERS = 2
@@ -8,30 +8,28 @@ MAX_PLAYERS = 2
 
 class Game:
     def __init__(self, board: Board = Board(10, 4)) -> None:
-        self.players: List[Player] = []
+        self.players: Dict = {}
         self.board: Board = board
         self.cnx: int = 0
 
-    def get_player(self, id: int) -> Player:
-        if id < 0 or id >= MAX_PLAYERS:
-            raise ValueError("Invalid ID")
-        else:
-            return self.players[id]
+    def get_player(self, player: str) -> Player:
+        return self.players[player]
 
-    def add_player(self) -> None:
+    def add_player(self) -> Player:
         if self.cnx == MAX_PLAYERS:
-            return
+            return None
 
         new_player = Player(PLAYER_NAMES[self.cnx])
         self.cnx += 1
-        self.players.append(new_player)
+        self.players[new_player.name] = new_player
+        return new_player
 
     def handle_pick(self, player, row, col):
         if row < 1 or row > self.board.n:
             raise ValueError(
                 f"Invalid row selection, must be between 1 and {self.board.n}"
             )
-        elif col < 1 or row > self.board.n:
+        elif col < 1 or col > self.board.n:
             raise ValueError(
                 f"Invalid column selection, must be between 1 and {self.board.n}"
             )
@@ -39,4 +37,5 @@ class Game:
             value = self.board.pick(row, col)
             if value.isnumeric():
                 player.add_score(int(value))
-                return player.score
+                return value
+            return value
--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3.11
-from Board import Board
-from Player import Player
+from Game import Game
 from asyncio import run, start_server, StreamReader, StreamWriter
 
 # from struct import pack, unpack
@@ -9,9 +8,7 @@ import struct
 BUF_SIZE = 4
 HOST = ""
 PORT = 12345
-PLAYER_NAMES = ["One", "Two"]
-cnx = 0
-board = Board(10, 4)
+game = Game()
 
 
 # Send the length of the player name to the connected client as an unsigned short
@@ -28,69 +25,39 @@ async def send_player(player, writer) -> None:
 
 # Using a global variable, keep track of which player is connecting to server (One or Two)
 # Return a string representing which player is connected.
-def get_player() -> str:
-    global cnx
-    return PLAYER_NAMES[cnx]
-
-
 async def asyncio_game_logic(reader: StreamReader, writer: StreamWriter) -> None:
-    # connection #
-    global cnx
-    # board for players
-    global board
-    if cnx <= 2:
-        player = Player(get_player())
-        await send_player(player, writer)
-        print(f"Connection received, {writer.get_extra_info("peername")}")
-        cnx += 1
-        while cnx > 0:
-            # Client is sending 1 byte of data
-            try:
-                data = await reader.read(1)
-                message = struct.unpack("!B", data)[0]
-                c_row = get_row(int(message))
-                if c_row < 1 or c_row > 10:
-                    raise ValueError("Selection must be between 1 and 10.")
-                c_col = get_col(int(message))
-                if c_col < 1 or c_col > 10:
-                    raise ValueError("Selection must be between 1 and 10.")
-                addr = writer.get_extra_info("peername")
-                print(f"Received {message}, {hex(message)} from {addr}")
-                value = board.pick(c_row, c_col)
-                if "Empty" == value:
-                    print("\nEmpty")
-                elif value.isnumeric():
-                    player.add_score(int(value))
-                    print("Found :" + value)
-                else:
-                    print(value)
-                print(board)
-                print(player)
-                packet = struct.pack("!HH", player.score, 0)
-                writer.write(packet)
-            except struct.error:
-                print("Error unpacking message")
-                packet = struct.pack("!B", "Invalid data found in packet.")
-                writer.write(packet)
-                cnx -= 1
-            except ValueError:
-                print("Player selected invalid coordinates.")
-                packet = struct.pack(
-                    "!B",
-                    "Invalid coordinates, row and column must be between 1 and 10.",
-                )
-                writer.write(packet)
-                cnx -= 1
-            except:
-                print("An error has occured")
-                packet = struct.pack("!B", "Invalid data found in packet.")
-                writer.write(packet)
-                cnx -= 1
-            finally:
-                await writer.drain()
-    else:
-        writer.close()
-        await writer.wait_closed()
+    player = game.add_player()
+    await send_player(player, writer)
+    print(f"Connection received, {writer.get_extra_info("peername")}")
+    while True:
+        # Client is sending 1 byte of data
+        try:
+            data = await reader.read(1)
+            message = struct.unpack("!B", data)[0]
+            c_row = get_row(int(message))
+            c_col = get_col(int(message))
+            addr = writer.get_extra_info("peername")
+            print(f"Received {message}, {hex(message)} from {addr}")
+            scored = game.handle_pick(player, c_row, c_col)
+            packet = struct.pack("!HH", scored, 0)
+            writer.write(packet)
+        except struct.error:
+            print("Error unpacking message")
+            packet = struct.pack("!B", "Invalid data found in packet.")
+            writer.write(packet)
+        except ValueError:
+            print("Player selected invalid coordinates.")
+            packet = struct.pack(
+                "!B",
+                "Invalid coordinates, row and column must be between 1 and 10.",
+            )
+            writer.write(packet)
+        except:
+            print("An error has occured")
+            packet = struct.pack("!B", "Invalid data found in packet.")
+            writer.write(packet)
+        finally:
+            await writer.drain()
 
 
 async def asyncio_main() -> None:
